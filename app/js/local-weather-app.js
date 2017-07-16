2cb6da97df110ba11673a4719434b2cea7fc1ca6
Stefan van Groningen <groninge@hotmail.com>
2017-07-16 13:02:13 +0200

    Added local weather data to fill table

--- app/js/local-weather-app.js
+++ app/js/local-weather-app.js
@@ -3,9 +3,10 @@ var baseUrlWeather = "//api.openweathermap.org/data/2.5/weather?";
 var apiKey = "&appid=e20788d5fc9f29f44032bd1ac7ca73d3";
 var units = "&units=metric";
 var symbolFahrenheit = "&deg;F";
-var symbolCelsius = "&deg;C";
+var symbolCelsius = " &deg;C";
 var temperature;
 var symbolTemperature = symbolCelsius;
+var tbody = document.getElementById('tbody');
 
 // Google font function
 (function() {
@@ -20,6 +21,16 @@ var symbolTemperature = symbolCelsius;
   s.parentNode.insertBefore(link_element, s);
 })();
 
+// Helper functions
+function createNode(element) {
+  return document.createElement(element); // Create the type of element you pass in the parameters
+}
+
+function append(parent, el) {
+  return parent.appendChild(el); // Append the second parameter(element) to the first one
+}
+
+// Main
 var result = fetch('//freegeoip.net/json/').then(function(response) {
   return response.json();
 }).then(function(data) {
@@ -38,4 +49,22 @@ var result = fetch('//freegeoip.net/json/').then(function(response) {
   // I'm using the result variable to show that you can continue to extend the chain from the returned promise
 result.then(function(r) {
     console.log(r); // 2nd request result
+    // set url for weather icon
+    var icon = baseUrlIcon + r.weather[0].icon + ".png";
+    // convert epoch to human readable date
+    var d = new Date(r.dt * 1000);
+    var sunRise = new Date(r.sys.sunrise * 1000);
+    var sunSet = new Date(r.sys.sunset * 1000);
+    var tr = "<tr>";
+    tr += "<td colspan=2>Weather in " + r.name + ", " + r.sys.country + "<br>";
+    tr += "<img src=" + icon + ">" + r.main.temp + symbolCelsius + "|" + symbolFahrenheit + "<br>";
+    tr += r.weather[0].description + "<br>";
+    tr += d.toTimeString().slice(0,5) + d.toDateString().slice(3,10) + "</td></tr>";
+    tr += "<tr><td>Wind</td><td>" + r.wind.speed + " m/s, " + r.wind.deg + "</td></tr>";
+    tr += "<tr><td>Pressure</td><td>" + r.main.pressure + " hpa</td></tr>";
+    tr += "<tr><td>Humidity</td><td>" + r.main.humidity + " %</td></tr>";
+    tr += "<tr><td>Sunrise</td><td>" + sunRise.toTimeString().slice(0,5) + "</td></tr>";
+    tr += "<tr><td>Sunset</td><td>" + sunSet.toTimeString().slice(0,5) + "</td></tr>";
+
+    tbody.innerHTML += tr;
 });
